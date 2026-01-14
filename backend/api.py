30ff74cfd95eb33c836bc15c86883b48c6934292
Sohan Barik <sohanbarik227@gmail.com>
2026-01-15 03:27:49 +0530

    sdglhnsbdgjlnb

--- backend/api.py
+++ backend/api.py
@@ -112,4 +112,4 @@ async def report_incident(
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
